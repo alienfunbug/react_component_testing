fc8110f04c12f474c38a0dab6d8757212c6ed289
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2021-09-04 05:00:09 -0700

    Solved json data mapping

--- catalogue/src/App.js
+++ catalogue/src/App.js
@@ -1,12 +1,31 @@
-
-import './App.css';
+import "./App.css";
+import characterData from "./data/positons.json";
 
 function App() {
+  const currentPosition = "All-Stars";
+  const nextPosition = "Centers";
+
+  const testMap = characterData.map((n) => {
+    if (n.position === nextPosition) {
+      console.log("\n n.position IS")
+      console.log(n.position)
+
+
+      n.players.map((player) =>{
+        console.log({player})
+      })
+    }
+  });
+
   return (
     <div className="App">
-
-          Learn React
-
+      {console.log("\n current position is ")}
+      
+      {console.log(currentPosition)}
+  
+   
+      {console.log("\n full test map is")}
+      {console.log(testMap)}
     </div>
   );
 }
